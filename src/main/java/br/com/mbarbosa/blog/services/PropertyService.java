929571f8e53663cb28aec5077677d2446e2bb124
Marcos Barbosa <barbosa.mg@gmail.com>
2020-03-01 20:43:13 -0300

    Método para obter endereço do diretório de upload

--- src/main/java/br/com/mbarbosa/blog/services/PropertyService.java
+++ src/main/java/br/com/mbarbosa/blog/services/PropertyService.java
@@ -1,4 +1,17 @@
 package br.com.mbarbosa.blog.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PropertyService {
+
+    @Autowired
+    private Environment env;
+
+    public String getUploadDir() {
+        return env.getProperty("blog.upload_dir");
+    }
+
 }
